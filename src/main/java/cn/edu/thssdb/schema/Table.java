e2e238d82655498cf5f9852ebc1354f4ad9f5ad7
yenjuc <48012444+yenjuc@users.noreply.github.com>
2021-05-07 18:22:26 +0800

    Table.update draft

--- src/main/java/cn/edu/thssdb/schema/Table.java
+++ src/main/java/cn/edu/thssdb/schema/Table.java
@@ -4,6 +4,7 @@ import cn.edu.thssdb.index.BPlusTree;
 import cn.edu.thssdb.utils.Pair;
 import cn.edu.thssdb.exception.OperateTableWithNullException;
 import cn.edu.thssdb.exception.SchemaLengthMismatchException;
+import cn.edu.thssdb.exception.ExceedSchemaLengthException;
 
 import javax.lang.model.type.ArrayType;
 import java.util.ArrayList;
@@ -45,17 +46,26 @@ public class Table implements Iterable<Row> {
     }
   }
 
-  private void checkLen(ArrayList<Column> columns, ArrayList<Entry> entries){
+  private void checkLen(ArrayList<Column> columns, ArrayList<Entry> entries, boolean equal){
     int schemaLen = this.columns.size();
     int columnsLen = columns.size();
     int entriesLen = entries.size();
-    if(columnsLen != schemaLen){
-      throw new SchemaLengthMismatchException(schemaLen, columnsLen, "columns");
-    }else if(entriesLen != schemaLen){
-      throw new SchemaLengthMismatchException(schemaLen, entriesLen, "entries");
+    if(equal){
+      if(columnsLen != schemaLen){
+        throw new SchemaLengthMismatchException(schemaLen, columnsLen, "columns");
+      }else if(entriesLen != schemaLen){
+        throw new SchemaLengthMismatchException(schemaLen, entriesLen, "entries");
+      }
+    }else{
+      if(columnsLen > schemaLen){
+        throw new ExceedSchemaLengthException(schemaLen, columnsLen, "columns");
+      }else if(entriesLen > schemaLen) {
+        throw new ExceedSchemaLengthException(schemaLen, entriesLen, "entries");
+      }else if(columnsLen != entriesLen){
+        throw new SchemaLengthMismatchException(columnsLen, entriesLen, "columns");
+      }
     }
   }
-
 
   /**
    *  功能：传入欲查询记录主 entry，返回对应的一行记录
@@ -81,7 +91,7 @@ public class Table implements Iterable<Row> {
     // check whether the length of columns and entries is preferable
     try{
       checkNull(columns, entries);
-      checkLen(columns, entries);
+      checkLen(columns, entries, true);
     }catch (Exception e){
       throw e;
     }
@@ -97,10 +107,9 @@ public class Table implements Iterable<Row> {
 
   /**
    *  功能：提供待删除记录的主 entry，将对应记录自 index 中删除
-   *  参数：entry为待删除记录的 entry
+   *  参数：entry为待删除记录的主 entry
    */
   public void delete(Entry entry) {
-    // TODO
     try{
       index.remove(entry);
     }catch (Exception e){
@@ -108,8 +117,34 @@ public class Table implements Iterable<Row> {
     }
   }
 
-  public void update() {
-    // TODO
+  /**
+   *  功能：提供待修改记录的主 entry，将根据传入参数修改 row
+   *  参数：entry为待删除记录的主 entry
+   */
+  public void update(Entry primaryEntry, ArrayList<Column> columns, ArrayList<Entry> entries) {
+    // check whether there is null columns or entries
+    // check whether the length of columns and entries is preferable
+    try{
+      checkNull(columns, entries);
+      checkLen(columns, entries, false);
+    }catch (Exception e){
+      throw e;
+    }
+
+    Row row = this.get(primaryEntry);
+    int columnsLen = columns.size();
+    int schemaIndex = 0, columnIndex = 0;
+    for(; columnIndex < columnsLen; columnIndex++){
+      while(columns.get(columnIndex).compareTo(this.columns.get(schemaIndex)) != 0){
+        ++schemaIndex;
+      }
+      row.entries.get(schemaIndex).value = entries.get(schemaIndex).value;
+    }
+    try {
+      index.update(primaryEntry, row);
+    }catch(Exception e){
+      throw e;
+    }
   }
 
   private void serialize() {
