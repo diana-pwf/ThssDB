61ef9cb8c6ec9dfb99cf64dcb09549d882f1a3b6
diana-pwf <diana_pwf@163.com>
2021-05-15 16:50:56 +0800

    初步完成Manager类 增加DatabaseNotExistException

--- src/main/java/cn/edu/thssdb/schema/Manager.java
+++ src/main/java/cn/edu/thssdb/schema/Manager.java
@@ -1,32 +1,84 @@
 package cn.edu.thssdb.schema;
 
+import cn.edu.thssdb.exception.DatabaseNotExistException;
 import cn.edu.thssdb.server.ThssDB;
 
+import javax.xml.crypto.Data;
 import java.util.HashMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Manager {
   private HashMap<String, Database> databases;
   private static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+  private Database currentDatabase;
 
   public static Manager getInstance() {
     return Manager.ManagerHolder.INSTANCE;
   }
 
   public Manager() {
-    // TODO
+    databases = new HashMap<String, Database>();
+    currentDatabase = null;
   }
 
-  private void createDatabaseIfNotExists() {
-    // TODO
+  private void createDatabaseIfNotExists(String databaseName) {
+    try{
+      lock.writeLock().lock();
+      // 若哈希表中没有该数据库名称，则新建并加入
+      if (!databases.containsKey(databaseName)) {
+        Database database = new Database(databaseName);
+        databases.put(databaseName, database);
+
+        if (currentDatabase == null) {
+          currentDatabase = database;
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      lock.writeLock().unlock();
+    }
   }
 
-  private void deleteDatabase() {
-    // TODO
+  private void deleteDatabase(String databaseName) {
+    try{
+      lock.writeLock().lock();
+      // 判断数据库是否存在
+      if (!databases.containsKey(databaseName)) {
+        throw new DatabaseNotExistException();
+      }
+
+      Database database = databases.get(databaseName);
+      database.drop();
+
+      //TODO: 取决于Database类中的drop函数实现
+      database = null;
+      databases.remove(databaseName);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      lock.writeLock().unlock();
+    }
+
   }
 
-  public void switchDatabase() {
-    // TODO
+  public void switchDatabase(String databaseName) {
+    try{
+      lock.readLock().lock();
+
+      // 判断数据库是否存在
+      if (!databases.containsKey(databaseName)) {
+        throw new DatabaseNotExistException();
+      }
+
+      currentDatabase = databases.get(databaseName);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      lock.readLock().unlock();
+    }
   }
 
   private static class ManagerHolder {
@@ -35,4 +87,11 @@ public class Manager {
 
     }
   }
+
+  public Database getCurrentDatabase() {
+    return currentDatabase;
+  }
+
 }
+
+// TODO: persist recover quit
