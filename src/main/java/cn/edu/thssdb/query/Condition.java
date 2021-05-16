8ddfc99a3fe6eb18f87dd938bb4e5f15b8e6c84e
yenjuc <48012444+yenjuc@users.noreply.github.com>
2021-05-16 21:26:33 +0800

    Add temp Condition Class

---
+++ src/main/java/cn/edu/thssdb/query/Condition.java
@@ -0,0 +1,15 @@
+package cn.edu.thssdb.query;
+
+
+// FIXME:
+
+import cn.edu.thssdb.schema.Row;
+
+public class Condition {
+    public Condition mLeft;
+    public Condition mRight;
+
+    public boolean JudgeCondition(Row row){
+        return true;
+    }
+}
