6a8b6a73395450681c4b6380e13101e4cd3b8c57
Qiqi-yu <yqq17thu@163.com>
2021-05-17 01:33:20 +0800

    persist and recover for database and manager,change the format of file storage.

--- src/main/java/cn/edu/thssdb/schema/Manager.java
+++ src/main/java/cn/edu/thssdb/schema/Manager.java
@@ -2,8 +2,10 @@ package cn.edu.thssdb.schema;
 
 import cn.edu.thssdb.exception.DatabaseNotExistException;
 import cn.edu.thssdb.server.ThssDB;
+// import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import javax.xml.crypto.Data;
+import java.io.*;
 import java.util.HashMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -19,6 +21,13 @@ public class Manager {
   public Manager() {
     databases = new HashMap<String, Database>();
     currentDatabase = null;
+    try
+    {
+      this.recover();
+    }
+    catch (Exception e){
+      System.out.println("Error occurs when manager recovers data!");
+    }
   }
 
   private void createDatabaseIfNotExists(String databaseName) {
@@ -90,6 +99,37 @@ public class Manager {
     return currentDatabase;
   }
 
-}
+  public void persist() throws IOException {
+    File file = new File("manager.data");
+    FileOutputStream fop = new FileOutputStream(file);
+    OutputStreamWriter writer = new OutputStreamWriter(fop);
+    // 将数据库名称存储到manager.data中
+    for(String databaseName : databases.keySet()){
+      writer.write(databaseName+'\n');
+    }
+    writer.close();
+    fop.close();
+  }
 
-// TODO: persist recover quit
+  public void recover() throws IOException {
+    File file = new File("manager.data");
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    while (reader.readLine()!=null) {
+       String databaseName = reader.readLine();
+       createDatabaseIfNotExists(databaseName);
+    }
+    reader.close();
+  }
+
+  public void  quit(){
+    lock.writeLock().lock();
+    try{for(Database database : databases.values()){
+      database.quit();
+    }
+      persist();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
