b446a0ce347442efb22192719447c17227c9da43
yenjuc <48012444+yenjuc@users.noreply.github.com>
2021-05-28 00:37:32 +0800

    增加处理数据库、表创建等SQL语句解析

--- src/main/java/cn/edu/thssdb/query/QueryResult.java
+++ src/main/java/cn/edu/thssdb/query/QueryResult.java
@@ -12,12 +12,27 @@ public class QueryResult {
   private List<MetaInfo> metaInfoInfos;
   private List<Integer> index;
   private List<Cell> attrs;
+  private boolean isQueryResult;
+  private String message;
 
+
+  // 查询结果
   public QueryResult(QueryTable[] queryTables) {
+    isQueryResult = true;
     // TODO
     this.index = new ArrayList<>();
     this.attrs = new ArrayList<>();
   }
+
+  // 执行结果
+  public QueryResult(String msg){
+    isQueryResult = false;
+    message = msg;
+  }
+
+  // 判断 QueryResult 返回的是否为查询结果，根据结果的不同使用不同的解析方式
+  public boolean getIsQueryResult() { return isQueryResult;}
+  public String getMessage() { return message;}
 
   public static Row combineRow(LinkedList<Row> rows) {
     // TODO
