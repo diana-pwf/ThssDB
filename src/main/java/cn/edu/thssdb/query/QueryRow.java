f67cc53f8e3e5cb7f1b43f6b4cf13cdfe6ddcb31
Qiqi-yu <yqq17thu@163.com>
2021-05-31 12:11:06 +0800

    modify QueryRow

--- src/main/java/cn/edu/thssdb/query/QueryRow.java
+++ src/main/java/cn/edu/thssdb/query/QueryRow.java
@@ -1,29 +1,35 @@
 package cn.edu.thssdb.query;
 
+import cn.edu.thssdb.parser.SQLParser;
 import cn.edu.thssdb.schema.Row;
 import cn.edu.thssdb.schema.Table;
+import sun.awt.AWTAccessor;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 // Notice:
 // 为什么要扩展QueryRow？
-// JOIN可能带来column和table对应关系——用MetaInfo来存储这部分
+// JOIN可能带来column和table对应关系——用MetaInfoList来存储这部分
 // 同时增加calColumnComparer来计算table.column的值->同样需要column和table的对应关系
 public class QueryRow extends Row {
-    private ArrayList<Table> tableList;
-    QueryRow(Table table,Row row){
+    private ArrayList<MetaInfo> MetaInfoList;
+    QueryRow(MetaInfo metaInfo, Row row){
         super();
-        tableList = new ArrayList<>();
-        tableList.add(table);
+        MetaInfoList = new ArrayList<>();
+        MetaInfoList.add(metaInfo);
         entries.addAll(row.getEntries());
     }
     // 此处采用LinkedList是因为在QueryTable中使用LinkedList更易于实现队列，笛卡尔积
     // tables和rows应该是对应的关系
-    QueryRow(ArrayList<Table> tables, LinkedList<Row> rows){
+    /**
+     *  构造QueryRow
+     * @param metaInfo 与row对应的table的信息
+     */
+    QueryRow(ArrayList<MetaInfo> metaInfo , LinkedList<Row> rows){
         super();
-        tableList = new ArrayList<>();
-        tableList.addAll(tables);
+        metaInfo = new ArrayList<>();
+        metaInfo.addAll(metaInfo);
         for(Row row:rows){
             entries.addAll(row.getEntries());
         }
